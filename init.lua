1f92798d0daed8c4880d3c0b0ac4cd691694b4d6
rawnly <rawnly@users.noreply.github.com>
2022-12-20 15:48:35 +0100

    feat: nvim-chad statusbar

--- init.lua
+++ init.lua
@@ -28,6 +28,46 @@ local config = {
   -- Set colorscheme to use
   colorscheme = "carbonfox",
 
+  icons = {
+    VimIcon = "",
+    ScrollText = "",
+    GitBranch = "",
+    GitAdd = "",
+    GitChange = "",
+    GitDelete = "",
+  },
+
+  heirline = {
+    -- define the separators between each section
+    separators = {
+      left = { "", " " }, -- separator for the left side of the statusline
+      right = { " ", "" }, -- separator for the right side of the statusline
+    },
+    -- add new colors that can be used by heirline
+    colors = function(hl)
+      -- use helper function to get highlight group properties
+      local comment_fg = astronvim.get_hlgroup("Comment").fg
+      hl.git_branch_fg = comment_fg
+      hl.git_added = comment_fg
+      hl.git_changed = comment_fg
+      hl.git_removed = comment_fg
+      hl.blank_bg = astronvim.get_hlgroup("Folded").fg
+      hl.file_info_bg = astronvim.get_hlgroup("Visual").bg
+      hl.nav_icon_bg = astronvim.get_hlgroup("String").fg
+      hl.nav_fg = hl.nav_icon_bg
+      hl.folder_icon_bg = astronvim.get_hlgroup("Error").fg
+      return hl
+    end,
+    attributes = {
+      mode = { bold = true },
+    },
+    icon_highlights = {
+      file_icon = {
+        statusline = false,
+      },
+    },
+  },
+
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   options = {
     opt = {
