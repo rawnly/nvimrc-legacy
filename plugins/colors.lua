0a6a59c692b30b2b1ba3e0e4d6aec62743587266
rawnly <rawnly@users.noreply.github.com>
2023-03-16 17:47:45 +0100

    new themes

--- plugins/colors.lua
+++ plugins/colors.lua
@@ -5,8 +5,8 @@ return {
   "folke/tokyonight.nvim",
   "cocopon/iceberg.vim",
   "nikolvs/vim-sunbather",
-  "EdenEast/nightfox.nvim",
   "Mofiqul/adwaita.nvim",
   "sainnhe/everforest",
   "rebelot/kanagawa.nvim",
+  "cormacrelf/vim-colors-github",
 }
