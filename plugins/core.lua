ef5b6dfb04ea7f2b3d102b4c77e273dff8371ac2
rawnly <rawnly@users.noreply.github.com>
2023-05-02 10:31:12 +0200

    fix: cmp C-j / C-k

--- plugins/core.lua
+++ plugins/core.lua
@@ -20,6 +20,17 @@ return {
       return opts
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require "cmp"
+
+      opts.mapping["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }, { "i", "c" })
+      opts.mapping["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }, { "i", "c" })
+
+      return opts
+    end,
+  },
   -- {
   --   "nvim-neo-tree/neo-tree.nvim",
   --   opts = function(_, opts)
