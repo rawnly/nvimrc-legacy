3eff215d9b0b865b412fc5bd3894f6fb1837b281
rawnly <rawnly@users.noreply.github.com>
2023-10-18 15:23:19 +0200

    fix: fixed mappings of cmp

--- plugins/core.lua
+++ plugins/core.lua
@@ -52,8 +52,8 @@ return {
     opts = function(_, opts)
       local cmp = require "cmp"
 
-      opts.mapping["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }, { "i", "c" })
-      opts.mapping["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }, { "i", "c" })
+      opts.mapping["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }
+      opts.mapping["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }
 
       return opts
     end,
