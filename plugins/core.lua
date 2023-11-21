d15d3b6b949e0235ab8669282bc02c4e5fb685c6
rawnly <rawnly@users.noreply.github.com>
2023-11-21 12:53:16 +0100

    setup neotree
    Signed-off-by: rawnly <rawnly@users.noreply.github.com>

--- plugins/core.lua
+++ plugins/core.lua
@@ -29,18 +29,6 @@ return {
         "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
       }
 
-      local alien_simple = {
-        "     ___",
-        " ___/   \\___",
-        "/   '---'   \\",
-        "'--_______--'",
-        "     / \\",
-        "    /   \\",
-        "    /\\O/\\",
-        "    / | \\",
-        "    // \\\\",
-      }
-
       -- customize the dashboard header
       opts.section.header.val = alien
 
@@ -89,13 +77,69 @@ return {
     },
   },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    config = function()
+      require("neo-tree").setup {
+        nesting_rules = {
+          ["go"] = {
+            pattern = "(.*)%.go$",
+            files = { "%1_test.go" },
+          },
+          [".env"] = {
+            pattern = "^%.env$",
+            files = { "*.env", ".env.*", ".envrc", "env.d.ts" },
+          },
+          ["docker"] = {
+            pattern = "^dockerfile$",
+            ignore_case = true,
+            files = { ".dockerignore", "docker-compose.*", "dockerfile*" },
+          },
+          ["ts"] = {
+            pattern = "(.+)%.ts$",
+            files = { "%1.test.ts", "%1.spec.ts" },
+          },
+          ["js"] = {
+            pattern = "(.+)%.js$",
+            files = { "%1.js.map", "%1.min.js", "%1.d.ts" },
+          },
+          ["next-config"] = {
+            pattern = "next.config.js",
+            files = {
+              "next-env.d.ts",
+              ".next",
+            },
+          },
+          ["tsconfig"] = {
+            pattern = "^tsconfig%.json$",
+            files = {
+              "tsconfig.*.json",
+            },
+          },
+          ["package"] = {
+            pattern = "^package%.json$",
+            files = {
+              "package-lock.json",
+              "pnpm*",
+              "yarn*",
+              ".eslint*",
+              ".prettier*",
+              "*.config.json",
+              ".npm*",
+              "bun.lockb",
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       indent = {
         enable = true,
-        disable = {
-          "python",
-        },
+        -- disable = {
+        --   "python",
+        -- },
       },
     },
   },
