d0ac5dc800db8cae5682a60dbe674c550ed662d8
rawnly <rawnly@users.noreply.github.com>
2023-11-21 12:53:37 +0100

    cleanup
    Signed-off-by: rawnly <rawnly@users.noreply.github.com>

--- options.lua
+++ options.lua
@@ -1,7 +1,6 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
@@ -11,7 +10,6 @@ return {
   },
   g = {
     enfocado_style = "neon",
-    -- material_style = "deep ocean",
     mapleader = " ", -- sets vim.g.mapleader
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true, -- enable completion at start
@@ -20,21 +18,7 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     heirline_bufferline = true,
-    -- astro_stylus = "enable",
-    -- astro_typescript = "enable",
     diagnostics_enabled = true, -- enable diagnostics at start
     status_diagnostics_enabled = true, -- enable diagnostics in statusline
-    -- gruvbox_material_background = "hard",
-    -- ["sneak#prompt"] = "",
-    -- ["sneak#s_next"] = 1,
   },
 }
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
